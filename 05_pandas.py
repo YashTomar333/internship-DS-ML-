b0c3e20850dae9888013af8c295384d898251515
Yash <yashtomar138@gmail.com>
2024-07-23 16:23:33 +0530

    05_pandas.py added, Removing Duplicates

---
+++ 05_pandas.py
@@ -0,0 +1,30 @@
+# Removing Duplicates
+
+import pandas as pd
+data = {
+    'Name': ['John', 'Anna', 'John', 'Anna', 'John'],
+    'Age': [28, 24, 29, 24, 19],
+    'City': ['New York', 'Los Angeles', 'New York', 'Los Angeles', 'Chicago']
+}
+df = pd.DataFrame(data)
+print(df)
+
+print(df.duplicated())           #Checking for duplicates
+df1 = df.drop_duplicates()       #Removing duplicates
+print(df)
+print(df1)
+
+
+
+# Removing data that has same email
+
+data1 = {
+    "Name":["a","b","c"],
+    "email":["a@gmail.com","b@gmail.com","a@gmail.com"]
+}
+df2 = pd.DataFrame(data1)
+print(df2)
+
+print(df2.duplicated(subset="email"))
+df2 = df2.drop_duplicates(subset="email")
+print(df2)
